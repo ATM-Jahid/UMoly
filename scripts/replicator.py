beaaa340f567c32ff8ceaedbc5b022574ac879c8
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2021-07-12 23:42:56 -0400

    Add defect detection in replicator

--- scripts/replicator.py
+++ scripts/replicator.py
@@ -10,8 +10,9 @@ parser.add_argument('-a', '--tilt', default=310, metavar='', help='lattice tilt 
 parser.add_argument('-x', '--xdim', default=50, metavar='', help='x dimension length', type=int)
 parser.add_argument('-y', '--ydim', default=200, metavar='', help='y dimension length', type=int)
 parser.add_argument('-z', '--zdim', default=12, metavar='', help='z dimension length', type=int)
-parser.add_argument('-m', '--moly', default='0.22', metavar='', help='Molybdenum portion')
+parser.add_argument('-M', '--moly', default='0.22', metavar='', help='Molybdenum portion')
 parser.add_argument('-t', '--temp', default='1200', metavar='', help='temperature in kelvin')
+parser.add_argument('-D', '--defect', default='none', metavar='', help='introduce interstitial/vacancy')
 parser.add_argument('-i', '--input', default='grain', metavar='', help='input file name')
 parser.add_argument('-o', '--output', default='niarg', metavar='', help='output file name')
 parser.add_argument('-n', '--number', default=1, metavar='', help='replication number', type=int)
@@ -25,20 +26,39 @@ xh = k * (int(args.xdim/k) + 1) / 2
 yh = k * (int(args.ydim/k) + 1) / 2
 zh = args.latt * (int(args.zdim/args.latt) + 1) / 2
 
+with open(f'in.{args.input}', 'r') as f:
+    jar = f.readlines()
+
+if args.defect in 'interstitial':
+    have_to_find = 'Interstitial'
+elif args.defect in 'vacancy':
+    have_to_find = 'Vacancy'
+
+for i in range(0, len(jar)):
+    if have_to_find in jar[i]:
+        UnStart = i
+        for j in range(i+2, len(jar)):
+            if "######" in jar[j]:
+                UnEnd = j
+                break
+        break
+
+for i in range(UnStart+3, UnEnd-1):
+    jar[i] = jar[i].replace('#', '', 1)
+
+jar = ''.join(jar)
+
+jar = jar.replace('LATT', f'{args.latt}', 1)
+jar = jar.replace('RUN', f'{p}', 1)
+jar = jar.replace('RISE', f'{q}', 1)
+jar = jar.replace('xHALF', f'{xh}', 1)
+jar = jar.replace('yHALF', f'{yh}', 1)
+jar = jar.replace('zHALF', f'{zh}', 1)
+jar = jar.replace('MOLY', f'{args.moly}', 1)
+jar = jar.replace('TEMP', f'{args.temp}', 1)
+
 for x in range(args.number):
-    with open(f'in.{args.input}', 'r') as f:
-        jar = f.read()
-
-    jar = jar.replace('LATT', f'{args.latt}', 1)
-    jar = jar.replace('RUN', f'{p}', 1)
-    jar = jar.replace('RISE', f'{q}', 1)
-    jar = jar.replace('xHALF', f'{xh}', 1)
-    jar = jar.replace('yHALF', f'{yh}', 1)
-    jar = jar.replace('zHALF', f'{zh}', 1)
-    jar = jar.replace('MOLY', f'{args.moly}', 1)
-    jar = jar.replace('SEED', f'{randint(10000,99999)}', 1)
-    jar = jar.replace('TEMP', f'{args.temp}', 1)
-    jar = jar.replace('GRAIN', f'{args.output}{x+1}')
-
+    foo = jar.replace('GRAIN', f'{args.output}{x+1}')
+    foo = foo.replace('SEED', f'{randint(10000,99999)}', 1)
     with open(f'in.{args.output}{x+1}', 'w') as f:
-        f.write(jar)
+        f.write(foo)
