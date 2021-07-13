96987c5c25a8718eb1c748dcbf45e55016111b53
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2021-07-13 04:07:59 -0400

    Add msd plotter

---
+++ scripts/msdPlotter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import matplotlib.pyplot as plt
+
+def escape(fileName):
+    with open(fileName, 'r') as f:
+        jar = f.readlines()
+    jar = jar[2:]
+
+    step = []
+    bar = []
+    for x in jar:
+        step.append(int(x.split()[0]))
+        bar.append(float(x.split()[9]))
+
+    plt.plot(step, bar, label=fileName[4:])
+
+if __name__ == "__main__":
+    folder = sys.argv[1:]
+    print(folder)
+    for file in folder:
+        escape(f'{file}')
+
+    plt.legend()
+    plt.show()
