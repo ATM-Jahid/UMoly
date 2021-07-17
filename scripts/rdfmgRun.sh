635101e4f7c820957246ae05a05585843d04caf8
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2021-07-17 18:55:49 -0400

    Tweak rdfmgRun.sh

--- scripts/rdfmgRun.sh
+++ scripts/rdfmgRun.sh
@@ -5,11 +5,11 @@
 #PBS -p gradq
 
 date
-ls
+pwd
+printf "\n[[ in.GRAIN ]]\n\n"
 
 module --ignore-cache load MPICH/3.2.1-GCC-7.2.0-2.29
-
 mpirun -np 32 /home/ahasan3/bin/lmp -i in.GRAIN
 
-echo "job finished"
+printf "\nJob finished!\n"
 date
