e2cdcd94bf938c7134da2c842698d70df6eb5b66
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2021-08-26 14:16:45 -0400

    Add linear regression script

---
+++ scripts/msdLinReg.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import statistics
+import numpy as np
+import matplotlib.pyplot as plt
+
+slopes = []
+
+def draw(fileName):
+    with open(fileName, 'r') as f:
+        jar = f.readlines()
+    jar = jar[12:]
+
+    step = []
+    bar = []
+
+    for x in jar:
+        step.append(float(x.split()[0]))
+        bar.append(float(x.split()[9]))
+
+    m, b = np.polyfit(step, bar, 1)
+    print(m, '\t', b)
+    slopes.append(m)
+
+    ordinate = []
+    for x in step:
+        ordinate.append(m*x + b)
+
+    tag = fileName[fileName.find('.')+1:]
+    plt.plot(step, ordinate, label=tag)
+
+def stat(slopeJar):
+    print("Mean: ", statistics.mean(slopeJar))
+    print("Stdev: ", statistics.stdev(slopeJar))
+
+if __name__ == "__main__":
+    folder = sys.argv[1:]
+    print(folder)
+    for file in folder:
+        draw(file)
+
+    stat(slopes)
+
+    plt.legend()
+    plt.show()
