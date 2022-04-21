aedec334bb49a31e3a558219e95458b0a8711b0d
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-04-21 01:57:07 -0400

    Add a crude buffer system for Xe

---
+++ scripts/xe_buff.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import copy
+import matplotlib.pyplot as plt
+
+def main():
+    files = sys.argv[1:]
+    print(files)
+
+    for file in files:
+        extract(file)
+
+def extract(file):
+    with open(file, 'r') as f:
+        jar = f.readlines()
+
+    buffDiff = 5
+    buffLen = 50
+    msd = [0]*buffLen
+    for b in range(0, len(jar)-buffLen, buffDiff):
+        xe_init = [float(x) for x in jar[b].split()]
+        for i in range(buffLen):
+            xe_curr = [float(x) for x in jar[b+i+1].split()]
+            d = 0
+            for l in range(3):
+                d += (xe_curr[l] - xe_init[l]) ** 2
+            msd[i] += d
+
+    numBuff = (len(jar) - buffLen) // buffDiff + 1
+    msd = [x / numBuff for x in msd]
+    plt.plot(msd)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
