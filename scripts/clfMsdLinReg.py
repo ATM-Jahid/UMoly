2e87fde9acabf7ef81496ff5069b8e130bc8334a
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-07-03 01:16:07 -0400

    Calculate total UMo diffusivity

--- scripts/clfMsdLinReg.py
+++ scripts/clfMsdLinReg.py
@@ -8,6 +8,11 @@ from statistics import mean, stdev
 def draw(fileName, writeFile, p):
     with open(fileName, 'r') as f:
         jar = f.readlines()
+
+    # get number of specific atoms from buff_msd
+    # the indices below are hardcoded
+    num_u = int(jar[1].split()[3][:-1])
+    num_mo = int(jar[1].split()[7])
     jar = jar[5:]
 
     u = []; mo = []
@@ -16,21 +21,29 @@ def draw(fileName, writeFile, p):
         u.append(float(tmp[0]))
         mo.append(float(tmp[1]))
     step = list(range(len(u)))
+    tot = [(num_u * x + num_mo * y) / (num_u + num_mo)
+            for x, y in zip(u, mo)]
 
     m1, b1 = np.polyfit(step, u, 1)
     m2, b2 = np.polyfit(step, mo, 1)
+    m3, b3 = np.polyfit(step, tot, 1)
 
     with open(writeFile, 'a') as f:
-        f.write(f'U: {m1:e} {b1:e}\nMo: {m2:e} {b2:e}\n')
+        f.write(f'U: {m1:e} {b1:e}\n' +
+                f'Mo: {m2:e} {b2:e}\n' +
+                f'Tot: {m3:e} {b3:e}\n')
 
     if p:
         ordinate1 = [m1*x+b1 for x in step]
         ordinate2 = [m2*x+b2 for x in step]
+        ordinate3 = [m3*x+b3 for x in step]
 
         plt.plot(step, u, 'r--', label='U')
         plt.plot(step, ordinate1, 'r')
         plt.plot(step, mo, 'g--', label='Mo')
         plt.plot(step, ordinate2, 'g')
+        plt.plot(step, tot, 'b-.', label='Tot')
+        plt.plot(step, ordinate3, 'b')
         plt.legend()
         plt.show()
 
--- scripts/diffuCal.py
+++ scripts/diffuCal.py
@@ -9,29 +9,35 @@ def extract(readFile, writeFile):
     jar = jar[2:]
     temp = int(readFile[readFile.find('at')+2:readFile.find('/')])
 
-    u = []; mo = []
+    u = []; mo = []; tot = []
     for line in jar:
         tmp = line.split()
         if 'U' in tmp[0]:
             u.append(float(tmp[1]))
         elif 'Mo' in tmp[0]:
             mo.append(float(tmp[1]))
+        elif 'Tot' in tmp[0]:
+            tot.append(float(tmp[1]))
 
     u_mean, u_std = mean(u), stdev(u)
     mo_mean, mo_std = mean(mo), stdev(mo)
+    tot_mean, tot_std = mean(tot), stdev(tot)
 
     xi = 2
-    to_pico = 1/0.002
+    to_pico = 1e-5/0.002
     to_si = 1e-8
 
     diff_u = 1 / 2 / xi * u_mean * to_pico * to_si
     devi_u = 1 / 2 / xi * u_std * to_pico * to_si
     diff_mo = 1 / 2 / xi * mo_mean * to_pico * to_si
     devi_mo = 1 / 2 / xi * mo_std * to_pico * to_si
+    diff_tot = 1 / 2 / xi * tot_mean * to_pico * to_si
+    devi_tot = 1 / 2 / xi * tot_std * to_pico * to_si
 
     with open(writeFile, 'a') as f:
-        f.write(f'{temp} {diff_u:.5e} {devi_u:.5e} ' +
-                f'{diff_mo:.5e} {devi_mo:.5e}\n')
+        f.write(f'{temp} {diff_u:e} {devi_u:e} ' +
+                f'{diff_mo:e} {devi_mo:e} ' +
+                f'{diff_tot:e} {devi_tot:e}\n')
 
 def main():
     # input "./clf_slopes" files
@@ -44,7 +50,7 @@ def main():
 
     with open(writeFile, 'a') as f:
         f.write(f'# Diffusivities for {fileName}\n' +
-                '# temp U_mean U_stdev Mo_mean Mo_stdev\n')
+                '# temp U_mean U_stdev Mo_mean Mo_stdev Tot_mean Tot_stdev\n')
 
     for file in files:
         extract(file, writeFile)
