4667b053687ff9eaa3c9ed193af03b8e50a59f67
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2023-11-22 21:10:05 -0500

    Introduce dt/reset in cascade input

--- scripts/sawtoothRun.sh
+++ scripts/sawtoothRun.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#PBS -N GRAIN
-#PBS -l select=6:ncpus=48:mpiprocs=12:ompthreads=4
-#PBS -l walltime=1:00:00
+#PBS -N __CASCADE
+#PBS -l select=6:ncpus=48:mpiprocs=48:ompthreads=1
+#PBS -l walltime=24:00:00
 #PBS -j oe
 #PBS -P mmm
 
 cd $PBS_O_WORKDIR
 echo $PBS_NODEFILE
-module load lammps/7jan22
+module load openmpi/4.1.5_ucx1.14.1
 
 date
 pwd
-printf "\n[[ in.GRAIN ]]\n\n"
+printf "\n[[ in.__CASCADE ]]\n\n"
 
-mpirun lmp -i in.GRAIN
+mpirun /home/hasaatmj/builds/lammps/build/lmp -i in.__CASCADE
 
 printf "\nJob finished!\n"
 date
